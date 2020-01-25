9cd6c0143e74953e6529002ec3c541a4a4a45bac
impedance <2275399@gmail.com>
2020-01-25 16:36:20 +0300

    seconde

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        <p>
+        Hello, friend! I am a basic component
+          </p>
+      </div>
+    );
+  }
+}
