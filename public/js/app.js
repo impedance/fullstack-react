22d7ce023fd998f01aaaaed0f43259c1a1359591
impedance <2275399@gmail.com>
2020-01-27 21:47:55 +0300

    add button

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,60 @@
 class ProductList extends React.Component {
+
+  handleProductUpVote(productId) {
+    console.log(productId + 'was upvoted')
+  }
   render() {
+    const products = Seed.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = products.map(product => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        handleProductUpVote={this.handleProductUpVote}
+      />
+    ));
     return (
-      <div className='ui unstackable items'>
-        <Product />
+      <div className="ui unstackable items">
+        {productComponents}
       </div>
-    );
+    )
   }
 }
 
 class Product extends React.Component {
+
   render() {
     return (
-      <div className='item'>
-        <div className='image'>
-          <img src='images/products/image-aqua.png' />
+      <div className="item">
+        <div className="image">
+          <img src={this.props.productImageUrl} />
         </div>
-        <div className='middle aligned content'>
-          <div className='description'>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two week\s.</p>
+        <div className="middle aligned content">
+          <div className="header">
+            <a>
+              <button onClick={(e) => this.props.handleProductUpVote(e)}>
+
+              <i className="large caret up icon" />
+              </button>
+            </a>
+            {this.props.votes}
           </div>
-          <div className='extra'>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className="extra">
             <span>Submitted by:</span>
             <img
-              className='ui avatar image'
-              src='images/avatars/daniel.jpg'
+              className="ui avatar image"
+              src={this.props.submitterAvatarUrl}
             />
           </div>
         </div>
@@ -33,7 +63,4 @@ class Product extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
+ReactDOM.render(<ProductList />, document.getElementById("content"));
